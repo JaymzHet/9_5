bf3517e72ced8dd220d7b5d0b7893bd389265798
JaymzHet <36802638+JaymzHet@users.noreply.github.com>
2018-03-14 20:41:56 +0100

    Corrected JS

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ var withButtonClass = document.getElementsByClassName('button');
 
 //console.log(withButtonClass);
 
-var ButtonList = withButtonClass.length;
+var buttonListLenght = withButtonClass.length;
 
-for (var i=1; i <= ButtonList; i++) {
-  alert(withButtonClass[i-1].innerText);
+for (var i=0; i < buttonListLenght; i++) {
+  alert(withButtonClass[i].innerText);
 };
 
